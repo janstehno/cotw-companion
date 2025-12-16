35fbdf5488e6f09cf31b9321c4b04d895c5b821f
Jan Stehno <honzastehno@email.cz>
2025-12-16 20:26:45 +0100

    Updated gradle and dependencies

--- android/app/src/main/kotlin/com/toastyapps/cotwcompanion/MainActivity.kt
+++ android/app/src/main/kotlin/com/toastyapps/cotwcompanion/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.toastyapps.cotwcompanion
 
+import android.os.Bundle
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+    }
 }
